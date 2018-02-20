9a323d3f14b4d6ab92d07d49d6d8076b373fa07b
tcscheurer <trevor.scheurer1@gmail.com>
2018-02-20 16:12:02 -0600

    Completed node-auth afternoon project

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+const strategy = require('./strategy');
+const request = require('request');
+
+const { clientID } = require('./config')
 
 const app = express();
 app.use( session({
-  secret: '@nyth!ng y0u w@nT',
+  secret: 'My-super_dooper..secureSecret',
   resave: false,
   saveUninitialized: false
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
+
+passport.serializeUser((user,done)=>{
+  done(null, user)
+})
+
+passport.deserializeUser((obj,done)=>{
+  console.log('DESERIAL: ', obj);
+  done(null,obj);
+})
+
+app.get('/login',passport.authenticate( 'auth0',
+  {
+  successRedirect: '/followers',
+  failureRedirect: '/login',
+  failureFlash: true,
+  connection: 'github'
+  }
+))
+
+app.get('/followers', (req,res)=>{
+  if ( req.user ) {
+    console.log('USER: ', req.user)
+    const FollowersRequest = {
+      url: `https://api.github.com/users/${req.user.nickname}/followers`, 
+      headers: {
+        'User-Agent': clientID
+      }
+    };
+
+    request(FollowersRequest, ( error, response, body ) => {
+      res.status(200).send(body);
+    });
+  } else {
+    console.log('ELSE FIREED')
+    res.redirect('/login');
+  }
+})
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
